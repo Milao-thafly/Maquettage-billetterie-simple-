84f35819aca0319a8cc05f5f9d63a7fa61d90aba
martin boizard <martinboizard@yahoo.fr>
2025-07-17 14:06:21 +0200

    Toute les entités sont prête a être créer

--- dist/main.js
+++ dist/main.js
@@ -1,27 +1,27 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', () => {
-  const btnClasse = document.getElementById('btnClasse');
-  const btnPays = document.getElementById('btnPays');
-  const inputClasse = document.getElementById('inputClasse');
-  const inputPays = document.getElementById('inputPays');
+// "use strict";
+// document.addEventListener('DOMContentLoaded', () => {
+//   const btnClasse = document.getElementById('btnClasse');
+//   const btnPays = document.getElementById('btnPays');
+//   const inputClasse = document.getElementById('inputClasse');
+//   const inputPays = document.getElementById('inputPays');
 
-  // Выбор класса
-  document.querySelectorAll('.dropdown-menu')[0].querySelectorAll('.dropdown-item').forEach(item => {
-    item.addEventListener('click', e => {
-      e.preventDefault();
-      const valeur = item.getAttribute('data-value');
-      btnClasse.textContent = valeur;
-      inputClasse.value = valeur;
-    });
-  });
+//   // Выбор класса
+//   document.querySelectorAll('.dropdown-menu')[0].querySelectorAll('.dropdown-item').forEach(item => {
+//     item.addEventListener('click', e => {
+//       e.preventDefault();
+//       const valeur = item.getAttribute('data-value');
+//       btnClasse.textContent = valeur;
+//       inputClasse.value = valeur;
+//     });
+//   });
 
-  // Выбор страны
-  document.querySelectorAll('.dropdown-menu')[1].querySelectorAll('.dropdown-item').forEach(item => {
-    item.addEventListener('click', e => {
-      e.preventDefault();
-      const valeur = item.getAttribute('data-value');
-      btnPays.textContent = valeur;
-      inputPays.value = valeur;
-    });
-  });
-});
+//   // Выбор страны
+//   document.querySelectorAll('.dropdown-menu')[1].querySelectorAll('.dropdown-item').forEach(item => {
+//     item.addEventListener('click', e => {
+//       e.preventDefault();
+//       const valeur = item.getAttribute('data-value');
+//       btnPays.textContent = valeur;
+//       inputPays.value = valeur;
+//     });
+//   });
+// });
