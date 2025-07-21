2c4de920aac9f18916021488c2b7fc600c64b4b5
Ingrid <ingridgusmao1@gmail.com>
2025-07-21 15:42:04 +0200

    ClasseMedium et ClasseEco

---
+++ src/classeco.ts
@@ -0,0 +1,13 @@
+import { AbsctractClass } from "./classAbstract.js";
+
+export class ClassEco extends AbsctractClass{
+    private static LABEL ="ECO"
+    private static VALUE ="ECO"
+    private static PRIX = 0.1
+    private static PERKS = "CHAUFFERU"
+
+    constructor(label: string, value: string, prix: number, perks: number){
+        super(label, value, prix, perks)
+    }
+
+}
