149420851f272c8098ce4767c2f0b5860881b478
martin boizard <martinboizard@yahoo.fr>
2025-07-21 15:48:38 +0200

    Commit avec la class abstract corrigée

--- src/class/classriche.ts
+++ src/class/classriche.ts
@@ -1,10 +1,10 @@
 import { AbsctractClass } from "./classAbstract.js";
 
 export class ClassRiche extends AbsctractClass{
-    private static LABEL =""
-    private static VALUE =""
-    private static PRIX = 100
-    private static PRECKS = ""
+    private static LABEL ="FIRST"
+    private static VALUE ="FIRST"
+    private static PRIX = 0.5
+    private static PRECKS = "CHAUFFERU"
 
     constructor(label: string, value: string, prix: number, percks: string){
         super(label, value, prix, percks)
