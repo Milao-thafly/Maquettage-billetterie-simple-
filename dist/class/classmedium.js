41a32f5cada73096cd417f0199db9f184af0b5f6
Ingrid <ingridgusmao1@gmail.com>
2025-07-23 14:58:43 +0200

    Classe Eco et Classe medium

--- dist/class/classmedium.js
+++ dist/class/classmedium.js
@@ -1,1 +1,12 @@
-"use strict";
+Classe;
+medium;
+import { AbsctractClass } from "./classAbstract.js";
+export class ClassMedium extends AbsctractClass {
+    constructor(label, value, prix, perks) {
+        super(label, value, prix, perks);
+    }
+}
+ClassMedium.LABEL = "BUSINESS";
+ClassMedium.VALUE = "BUSINESS";
+ClassMedium.PRIX = 0.25;
+ClassMedium.PERKS = "CHAUFFERU";
