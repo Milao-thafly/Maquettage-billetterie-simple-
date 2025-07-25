98a0219383194ceaa10ae23cf4d81111894a86ed
Martin Boizard <martinboizard@yahoo.fr>
2025-07-25 11:24:38 +0200

    Algo de calcul de prix

---
+++ src/class/payFromDistance.ts
@@ -0,0 +1,13 @@
+import { allClasses, ClassTrip } from "./classTrip.js";
+
+
+export class vaVoirLeMonde {
+payFromDistance( classValue: string, distance: number): number  {
+const selectedClass = allClasses.find(allClasses => allClasses.getValue() === classValue)!;
+
+const totalQuiFaitMal = selectedClass.getPrix() * distance
+return totalQuiFaitMal;
+}
+}
+const voyageVOYAGE = new vaVoirLeMonde();
+console.log(voyageVOYAGE.payFromDistance("riche", 340))
