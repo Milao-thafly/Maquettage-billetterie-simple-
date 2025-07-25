66c7f96ae8e07db2de47754e013a941c1aadd486
Ingrid <ingridgusmao1@gmail.com>
2025-07-25 17:06:53 +0200

    Recuperation

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,2 +1,13 @@
-export type Ville = "Paris" | "Londres" | "Berlin" | "Madrid" | "Rome" | "Lisbonne" | "Vienne" | 
-"Amasterdam" | "Bruxelles" | "Copenhague"
+import { Destination } from "../class/object"
+ export const destinations: Destination[] = [
+    { value: "pabuzzfeedris", distanceFromParis: 0 },
+    { value: "london", distanceFromParis: 340 },
+    { value: "berlin", distanceFromParis: 1050 },
+    { value: "madrid", distanceFromParis: 1250 },
+    { value: "rome", distanceFromParis: 1100 },
+    { value: "lisbon", distanceFromParis: 1450 },
+    { value: "vienna", distanceFromParis: 1230 },
+    { value: "amsterdam", distanceFromParis: 510 },
+    { value: "brussels", distanceFromParis: 300 },
+    { value: "copenhagen", distanceFromParis: 1380 },
+  ];
