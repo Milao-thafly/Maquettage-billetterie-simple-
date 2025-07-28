889e8d32382c89b9cdce6e2e4830141f063a99af
Martin Boizard <martinboizard@yahoo.fr>
2025-07-29 01:47:28 +0200

    Chemin entre toute les pages fonctionnel je peux mourir

--- src/confirmation.ts
+++ src/confirmation.ts
@@ -1,2 +1,31 @@
 import { loadFormData } from "./localStorage.js";
+const btnpasse = document.getElementById("btnpasse")
+// Affiche les infos du formulaire dans la div getInfo
+document.addEventListener("DOMContentLoaded", () => {
+    const data = loadFormData();
+    const logContainer = document.getElementById("getInfo");
 
+    if (!logContainer || !data) return;
+
+    logContainer.innerHTML = `
+        <ul class="list-group mb-3">
+            <li class="list-group-item">Nom : ${data.username}</li>
+            <li class="list-group-item">Prénom : ${data.first_name}</li>
+            <li class="list-group-item">Âge : ${data.age}</li>
+            <li class="list-group-item">Email : ${data.email}</li>
+            <li class="list-group-item">Téléphone : ${data.phone}</li>
+            <li class="list-group-item">Classe : ${data.classe}</li>
+            <li class="list-group-item">Destination : ${data.pays}</li>
+        </ul>
+        
+    `;
+})
+
+    
+
+
+if(btnpasse){
+    btnpasse.addEventListener("click", () => {
+    window.location.href = "paiement.html"
+    })  
+}
--- src/localStorage.ts
+++ src/localStorage.ts
@@ -1,15 +1,7 @@
 
-// export class FormStorage{
-//     static save(data: any): void {
-//     localStorage.setItem("formulaire", JSON.stringify(data))
-//     }
 
-//     static load(): any | null {
-//         const getform = localStorage.getItem("formulaire");
-//         return getform ? JSON.parse(getform): null;
-//     }
 
-// }
+
 export function saveFormData(data: any): void {
   localStorage.setItem("formulaire", JSON.stringify(data));
 }
@@ -18,3 +10,4 @@ export function loadFormData(): any | null {
   const getinfo = localStorage.getItem("formulaire");
   return getinfo ? JSON.parse(getinfo) : null;
 }
+
